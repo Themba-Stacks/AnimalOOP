959143a0f86cd8bec5e814cc6140d9aa2b62eea8
anele-manaka <anele.s.manaka@gmail.com>
2020-05-04 11:50:38 +0200

    anele commit

--- src/Animal.js
+++ src/Animal.js
@@ -11,4 +11,30 @@ class Animal {
         return (this.name + "eat")
     }
 }
+class Cat extends Animal {
+    constructor(name){
+        super(name);
+
+    }
+sound(){
+    return ("Cat meows")
+}
+}
+let cat = new Cat ();
+cat.name = "Stormy";
+console.log(cat.sound())
+console.log(cat.eat());
+
+class dog extends Animal {
+    constructor(name){
+        super(name);
+    }
+    sound(){
+        return ("Dog barks")
+    }
+}
+let dog = new Dog ();
+cat.name = "Rax";
+console.log(dog.sound())
+console.log(dog.eat());
 
